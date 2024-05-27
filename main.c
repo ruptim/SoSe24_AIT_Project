10e9cdaa6f58de9dee39843715b8214275756875
ruptim <timonrupelt@gmx.de>
2024-05-27 10:40:24 +0200

    added formatting of phydat_t values for GET Request e.g. '10 kPa'; added check of payload len for PUT request; fixed hello_world test code for PUT requests

--- main.c
+++ main.c
@@ -48,12 +48,7 @@ static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 // --
 
-// static ssize_t _encode_link(const coap_resource_t *resource, char *buf,
-//                             size_t maxlen, coap_link_encoder_ctx_t *context);
 static ssize_t _riot_board_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
-// static ssize_t _pressure_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
-
-static int _request_matcher(gcoap_listener_t *listener, const coap_resource_t **resource, coap_pkt_t *pdu);
 
 coap_resource_t *_resources;
 
@@ -75,187 +70,79 @@ coap_resource_t *_resources;
 //     return res;
 // }
 
-// custom response matcher: nötig??
-static int _request_matcher(gcoap_listener_t *listener,
-                            const coap_resource_t **resource,
-                            coap_pkt_t *pdu)
-{
-    uint8_t uri[CONFIG_NANOCOAP_URI_MAX];
-    int ret = GCOAP_RESOURCE_NO_PATH;
-
-    if (coap_get_uri_path(pdu, uri) <= 0)
-    {
-        /* The Uri-Path options are longer than
-         * CONFIG_NANOCOAP_URI_MAX, and thus do not match anything
-         * that could be found by this handler. */
-        return GCOAP_RESOURCE_NO_PATH;
-    }
-
-    coap_method_flags_t method_flag = coap_method2flag(
-        coap_get_code_detail(pdu));
-
-    for (size_t i = 0; i < listener->resources_len; i++)
-    {
-        *resource = &listener->resources[i];
-
-        int res = coap_match_path(*resource, uri);
-
-        /* URI mismatch */
-        if (res != 0)
-        {
-            continue;
-        }
-
-        /* potential match, check for method */
-        if (!((*resource)->methods & method_flag))
-        {
-            /* record wrong method error for next iteration, in case
-             * another resource with the same URI and correct method
-             * exists */
-            ret = GCOAP_RESOURCE_WRONG_METHOD;
-            continue;
-        }
-        else
-        {
-
-            return GCOAP_RESOURCE_FOUND;
-        }
-    }
-
-    return ret;
-}
-
-// char* format_value(phydat_t data){
-
-//     return "";
-// }
-
 /*
-
-*/
-size_t eval_device(coap_request_ctx_t *ctx, coap_pkt_t *pdu)
+ *   Handler for coap requests to read sensors or drive actuators via SAUL.
+ */
+static ssize_t _riot_board_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
 {
+
+    // size_t response_len = eval_device(ctx, pdu);
     char *dev_name = (char *)ctx->resource->context;
     saul_reg_t *dev = saul_reg_find_name(dev_name);
 
-
     /* read coap method type in packet */
     unsigned method_flag = coap_method2flag(coap_get_code_detail(pdu));
-    method_flag = ctx->resource->methods;
-    // method_flag++;
+    method_flag = ctx->resource->methods; // just for testing
 
     if (method_flag == COAP_GET)
     {
+        gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+        coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+        size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
         phydat_t data;
         char unit[10] = "";
+        char value[5] = ""; // Max length of 2^16 is 5 (65536)
 
+        /* get data */
         saul_reg_read(dev, &data);
+
+        /*get value*/
+        fmt_u16_dec(value, data.val[0]);
+
+        /*get unit*/
         phydat_unit_write(unit, sizeof(unit), data.unit);
 
+        /*get scale prefix*/
         char prefix = phydat_prefix_from_scale(data.scale);
-        printf("DATA READ: %d, %d, %c, %s\n", data.val[0], data.scale, prefix, unit);
-        // uint16_t value = data.val[0];
-        //TODO: convert value to correct output and fill reponse!!
 
-        
-                
-        char response[20]; 
+        /* build response string
+            => value + empty_space + prefix_char + unit
+        */
+        char response[strlen(value) + 1 + 1 + strlen(unit)];
+        sprintf(response, "%s %c%s", value, prefix, unit);
+
         memcpy(pdu->payload, response, strlen(response));
-        return strlen(response);
+        return resp_len + strlen(response);
     }
     else if (method_flag == COAP_PUT)
     {
-        char *response;
-        // TODO: check values
-        int state = (int) *pdu->payload;
-        phydat_t data = {{state, 0, 0}, UNIT_NONE, 0};
-        saul_reg_write(dev, &data);
-        response = "";
-        memcpy(pdu->payload, response, strlen(response));
-        return strlen(response);
+        if (pdu->payload_len == 1)
+        {
+            uint8_t state = (uint8_t)*pdu->payload;
+            phydat_t data = {{state, 0, 0}, UNIT_NONE, 0};
+            saul_reg_write(dev, &data);
+
+            return gcoap_response(pdu, buf, len, COAP_CODE_CHANGED);
+        }
+        else
+        {
+            return gcoap_response(pdu, buf, len, COAP_CODE_BAD_REQUEST);
+        }
     }
-    char *response;
-    response = "Request was not GET or PUT!";
-    memcpy(pdu->payload, response, strlen(response));
-    return strlen(response);
+
+    return 0;
 }
 
-static ssize_t _riot_board_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
-{
-    (void)ctx;
-    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-    coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
-    size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
-
-    uint8_t uri[CONFIG_NANOCOAP_URI_MAX];
-
-    if (coap_get_uri_path(pdu, uri) <= 0)
-    {
-        /* The Uri-Path options are longer than
-         * CONFIG_NANOCOAP_URI_MAX, and thus do not match anything
-         * that could be found by this handler. */
-        perror("URI TO BIG!");
-    }
-
-    size_t response_len = eval_device(ctx, pdu);
-
-    return resp_len + response_len;
-}
-
-// static ssize_t _pressure_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx) {
-//     (void)ctx;
-//     gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-//     coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
-//     size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
-
-//     phydat_t data;
-//     saul_reg_read(saul_reg_find_nth(4), &data);
-//     char str[20];
-
-//     sprintf(str, "Val: %hi", *data.val); // todo: correct formatting?
-//     /* Output is:
-//         Showing hex dump of text/plain;charset=utf-8 payload: Message can not be parsed as UTF-8
-//         00000000  56 61 6c 3a 20 30 00 1f  68 cf ff 1f 90 cf ff 1f  |Val: 0..h.......|
-//         00000010  00 00 00 00                                       |....|
-//         00000014
-//      */
-
-//     /* write the RIOT board name in the response buffer */
-//     if (pdu->payload_len >= sizeof(str)) {
-// //        memcpy(pdu->payload, RIOT_BOARD, strlen(RIOT_BOARD));
-//         memcpy(pdu->payload, str, sizeof(str));
-//         return resp_len + sizeof(str);
-//     }
-//     else {
-//         puts("gcoap_cli: msg buffer too small");
-//         return gcoap_response(pdu, buf, len, COAP_CODE_INTERNAL_SERVER_ERROR);
-//     }
-// }
-
-void server_init(gcoap_listener_t listener)
-{
-#if IS_USED(MODULE_GCOAP_DTLS)
-    int res = credman_add(&credential);
-    if (res < 0 && res != CREDMAN_EXIST)
-    {
-        /* ignore duplicate credentials */
-        printf("gcoap: cannot add credential to system: %d\n", res);
-        return;
-    }
-    sock_dtls_t *gcoap_sock_dtls = gcoap_get_sock_dtls();
-    res = sock_dtls_add_credential(gcoap_sock_dtls, GCOAP_DTLS_CREDENTIAL_TAG);
-    if (res < 0)
-    {
-        printf("gcoap: cannot add credential to DTLS sock: %d\n", res);
-    }
-#endif
-
-    gcoap_register_listener(&listener);
-}
-
+/*
+ * Initializes coap resources for every saul device found for the current hardware.
+ * The URI of a resource is created from the name of the corresponding device and the prefix URI_BASE.
+ */
 coap_resource_t *init_board_periph_resources(void)
 {
-    printf("Seaching devices!");
+    // printf("Seaching devices!");
+
+    /* count registered saul devices */
     int dev_count = 0;
     if (saul_reg != NULL)
     {
@@ -263,11 +150,13 @@ coap_resource_t *init_board_periph_resources(void)
 
         while (tmp)
         {
-            printf("[Init] Found device: \"%s\" %d\n", tmp->name, tmp->driver->type);
+            printf("[Init] Found device: \"%s\"\n", tmp->name);
             tmp = tmp->next;
             dev_count++;
         }
     }
+
+    /* create resource array and fill it with a resource per registered device */
     coap_resource_t *arrayPtr = malloc(dev_count * sizeof(coap_resource_t));
     if (dev_count > 0)
     {
@@ -302,37 +191,58 @@ coap_resource_t *init_board_periph_resources(void)
     return arrayPtr;
 }
 
+void server_init(gcoap_listener_t listener)
+{
+#if IS_USED(MODULE_GCOAP_DTLS)
+    int res = credman_add(&credential);
+    if (res < 0 && res != CREDMAN_EXIST)
+    {
+        /* ignore duplicate credentials */
+        printf("gcoap: cannot add credential to system: %d\n", res);
+        return;
+    }
+    sock_dtls_t *gcoap_sock_dtls = gcoap_get_sock_dtls();
+    res = sock_dtls_add_credential(gcoap_sock_dtls, GCOAP_DTLS_CREDENTIAL_TAG);
+    if (res < 0)
+    {
+        printf("gcoap: cannot add credential to DTLS sock: %d\n", res);
+    }
+#endif
+
+    gcoap_register_listener(&listener);
+}
+
+/*
+ *    function to test coap handler without coap
+ */
 static int hello_world(int argc, char **argv)
 {
     /* Suppress compiler errors */
     (void)argc;
     (void)argv;
 
-    int dev_id = 3;
+    int dev_id = 0;
 
     phydat_t data;
     saul_reg_t *dev = saul_reg_find_nth(dev_id);
     saul_reg_read(dev, &data);
-    // printf("%s: %d\n", dev->name, data.val[0]);
-    // LED_TOGGLE(0);
 
     const coap_resource_t rs = _resources[dev_id];
     coap_request_ctx_t context = {&rs, NULL, 0};
-    printf("Dev: %s, %d, %d\n", (char *)rs.context, rs.methods,dev->driver->type);
+    printf("Dev: %s, Class: %d, Method: %d,\n", (char *)rs.context, dev->driver->type, rs.methods);
 
-    
-    char pl[20] = "";
     coap_pkt_t pdu;
-    pdu.payload = (uint8_t*) pl;
-    pdu.payload_len = 0;
-    eval_device(&context,&pdu);
-    printf("%s\n",(char*)pdu.payload);
+    pdu.payload = (uint8_t *)(rs.methods == 4 ? !(bool)data.val[0] : 1); // if PUT, toggle value
+    pdu.payload_len = 1;
+    uint8_t buf[CONFIG_GCOAP_PDU_BUF_SIZE];
+
+    _riot_board_handler(&pdu, &buf[0], CONFIG_GCOAP_PDU_BUF_SIZE, &context);
+    printf("Payload: %s\n", (char *)pdu.payload);
     return 0;
 }
 
-// const shell_command_t shell_commands[] = {
-//     {"hello", "hello", hello_world},
-//     {NULL, NULL, NULL}};
+
+
 
 int main(void)
 {
@@ -343,10 +253,9 @@ int main(void)
         &_resources[0],
         size,
         GCOAP_SOCKET_TYPE_UNDEF,
-        // _encode_link,
         gcoap_encode_link,
         NULL,
-        _request_matcher};
+        NULL};
 
     shell_command_t shell_commands[] = {
         {"hello", "hello", hello_world},
