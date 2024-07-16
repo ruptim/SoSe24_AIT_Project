585574058194e1545c529f9ae4e5ae3fc744c171
Robin <44874951+r-grun@users.noreply.github.com>
2024-07-16 14:44:34 +0200

    Move questions to json file

--- project/frontend/app/game/page.tsx
+++ project/frontend/app/game/page.tsx
@@ -12,6 +12,7 @@ import {ConnectionContainer} from "@/app/game/connection/connection-container";
 import {Button} from "@nextui-org/button";
 import {Socket} from "socket.io-client";
 import {DefaultEventsMap} from "@socket.io/component-emitter";
+import questions from '../../config/questions.json'
 
 export default function GamePage() {
   const [appSocket, setSocket] = useState< Socket<DefaultEventsMap, DefaultEventsMap>>(socket)
@@ -54,52 +55,9 @@ export default function GamePage() {
     };
   }, []);
 
-  let questions: QuestionType[] = [
-    {
-      question: "Was ist das Internet der Dinge (IoT)?",
-      options: [
-        {optionText: "Option A", isCorrect: false},
-        {optionText: "Option B", isCorrect: true},
-        {optionText: "Option C", isCorrect: false},
-        {optionText: "Option D", isCorrect: false}
-      ],
-      answer:
-        "Das IoT ist ein Netzwerk physischer Objekte oder Personen, die mit Software, Elektronik, Netzwerken und Sensoren ausgestattet sind. Diese Objekte sammeln und tauschen Daten aus, um sie intelligenter und effizienter zu machen.",
-    },
-    {
-      question: "Was sind die grundlegenden Komponenten eines IoT-Systems?",
-      options: [
-        {optionText: "Option A", isCorrect: false},
-        {optionText: "Option B", isCorrect: true},
-        {optionText: "Option C", isCorrect: false},
-        {optionText: "Option D", isCorrect: true}
-      ],
-      answer:
-        "Die vier grundlegenden Komponenten sind Sensoren/Devices, Konnektivität, Datenverarbeitung und Benutzeroberfläche.",
-    },
-    {
-      question: "Was ist der Unterschied zwischen IoT und IIoT?",
-      options: [],
-      answer:
-        "IoT ist auf kundenorientierte Anwendungen ausgerichtet, während IIoT industrielle Anwendungen unterstützt. IIoT verwendet sowohl drahtgebundene als auch drahtlose Kommunikation und bietet hochwertige Daten.",
-    },
-    {
-      question: "Was ist Raspberry Pi?",
-      options: [],
-      answer:
-        "Raspberry Pi ist ein Computer, der ähnlich wie ein herkömmlicher Computer funktioniert. Er verfügt über Funktionen wie WLAN, GPIO-Pins und Bluetooth zur Kommunikation mit externen Geräten.",
-    },
-    {
-      question: "Wie läuft Raspberry Pi im Headless-Modus?",
-      options: [],
-      answer:
-        "Raspberry Pi kann im Headless-Modus über SSH betrieben werden. Das neueste Betriebssystem verfügt über einen integrierten VNC-Server für die Fernsteuerung.",
-    },
-  ];
-
   return (
     <div className="w-full max-w-screen-md min-w-full flex flex-col">
-      <QuestionContainer questions={questions}/>
+      <QuestionContainer questions={questions.questions}/>
       <Divider className={"mt-5 mb-5"}/>
       <ActivityContainer buzzers={buzzers}/>
       <div className="self-end">
